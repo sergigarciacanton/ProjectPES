6ce58a13c513c90858d3d64b2935ad9d8a50f796
unknown <sgarciac01@hotmail.com>
2021-12-26 21:01:37 +0100

    Comments added

--- app/controllers/Android.java
+++ app/controllers/Android.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 public class Android extends Controller {
 
+    //Here the username who sent the query to server is saved
     public static String connectedUser;
 
     @Before
@@ -20,12 +21,22 @@ public class Android extends Controller {
 
     public static void login(String mail, String password) {
         //localhost:9000/Android/login?mail=s@gmail.com&password=123
+
+        //Query asks for users given its credentials as parameters
         User user = User.find("byMailAndPassword", mail, password).first();
+
+        //In case there are no results, check if password given is wrong
         if (user == null) {
+            //Query asks for users given its username as parameter
             user = User.find("byMail", mail).first();
+
+            //In case there are no results, this username does not exist. Send error
             if (user == null) renderText("-1");
+
+            //If it does exist, password is wrong. Send error
             else renderText("-2");
         }
+        //If there is a result, credentials are well. Log in and return result
         else {
             connectedUser = mail;
             session.put("user", mail);
@@ -47,25 +58,39 @@ public class Android extends Controller {
 
     public static void getInbox(String mail, String inboxCode) {
         //localhost:9000/Android/getInbox?mail=j@gmail.com&inboxCode=main
+
+        //In case that user asks for mails sent by it, database query is different (asks for receivers giving sender as data)
         if(inboxCode.equals("sent")) {
+            //Query asks for message, receiver's mail and date for messages sent to the mail given as parameter
             Query query = JPA.em().createQuery("SELECT m.message, m.receiver.mail, m.date FROM Message_User m WHERE m.sender.mail " +
                             "LIKE :receiverMail")
                     .setParameter("receiverMail", mail);
             List<Message> messages = query.getResultList();
+
+            //In case there are no results, send error
             if (messages.size() == 0) renderText("-1");
+
+            //If there are results, cast them to JSON and send
             else {
                 Gson g = new Gson();
                 g = g.newBuilder().excludeFieldsWithoutExposeAnnotation().create();
                 renderJSON(g.toJson(messages));
             }
         }
+
+        //Rest of cases: ask for senders giving receiver
         else {
+            //Ask for message, sender's mail and date for messages sent by the mail given as parameter
             Query query = JPA.em().createQuery("SELECT m.message, m.sender.mail, m.date FROM Message_User m WHERE m.receiver.mail " +
                             "LIKE :receiverMail AND m.inbox LIKE :inboxCode")
                     .setParameter("receiverMail", mail)
                     .setParameter("inboxCode", inboxCode);
             List<Message> messages = query.getResultList();
+
+            //In case there are no results, send error
             if (messages.size() == 0) renderText("-1");
+
+            //If there are results, cast them to JSON and send
             else {
                 Gson g = new Gson();
                 g = g.newBuilder().excludeFieldsWithoutExposeAnnotation().create();
--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -9,6 +9,7 @@ import com.google.gson.Gson;
 
 public class Application extends Controller {
 
+    //Here the username who sent the query to server is saved
     public static String connectedUser;
 
     @Before
@@ -16,22 +17,37 @@ public class Application extends Controller {
         connectedUser = session.get("user");
     }
 
+    //Renders login webpage
     public static void index() {render();}
 
+    //Renders register webpage
     public static void indexRegister() {render();}
 
+    //Renders webpage to send messages
     public static void indexSendMessage() {renderTemplate("Application/sendMessage.html");}
 
+    //Renders webpage to delete the account
     public static void indexDeleteAccount() {renderTemplate("Application/deleteAccount.html");}
 
+    //Query for logging in
     public static void login(String mail, String password) {
         //localhost:9000/Application/login?mail=s@gmail.com&password=123
+
+        //Query asks for users given its credentials as parameters
         User user = User.find("byMailAndPassword", mail, password).first();
+
+        //In case there are no results, check if password given is wrong
         if (user == null) {
+            //Query asks for users given its username as parameter
             user = User.find("byMail", mail).first();
+
+            //In case there are no results, this username does not exist. Render corresponding error
             if (user == null) renderTemplate("errors/loginMailError.html");
+
+            //If it does exist, password is wrong. Render corresponding error
             else renderTemplate("errors/loginPasswordError.html");
         }
+        //If there is a result, credentials are well. Log in and render inbox template
         else {
             connectedUser = mail;
             session.put("user", mail);
@@ -39,97 +55,144 @@ public class Application extends Controller {
         }
     }
 
+    //Query for registering
     public static void register(String mail, String password, String fullName) {
         //localhost:9000/Application/register?mail=a@gmail.com&password=123&fullName=Albert
+
+        //Query asks for users given its username as parameter
         User user = User.find("byMail", mail).first();
+
+        //In case there are results, username does already exist. Render error
         if (user != null) {
             renderTemplate("errors/registerMailError.html");
         }
+        //If there are no results, user can be registered. Render message
         else {
             new User(mail, password, fullName).save();
             renderTemplate("messages/registerSuccess.html");
         }
     }
 
+    //Query for deleting account
     public static void deleteAccount(String mail, String password) {
+        //Query asks for users given its username as parameter
         User user = User.find("byMail", mail).first();
-            if (user != null) {
-                if (password.equals(user.password)) {
-                    if(user.sendersList != null){
-                        Message_User mu;
-                        for(int i = 0; i<user.sendersList.size(); i++) {
-                            mu = user.sendersList.get(i);
-                            mu.sender = null;
-                            mu.save();
-                        }
+
+        //If user exists, check if password is well
+        if (user != null) {
+
+            //If password is well, proceed to delete the account
+            if (password.equals(user.password)) {
+
+                //User references in sent messages must be previously deleted
+                if(user.sendersList != null){
+                    Message_User mu;
+                    for(int i = 0; i<user.sendersList.size(); i++) {
+                        mu = user.sendersList.get(i);
+                        mu.sender = null;
+                        mu.save();
                     }
-                    if(user.receiversList != null){
-                        Message_User mu;
-                        for(int i = 0; i<user.receiversList.size(); i++) {
-                            mu = user.receiversList.get(i);
-                            mu.receiver = null;
-                            mu.save();
-                        }
-                    }
-                    user.delete();
-                    renderTemplate("Application/index.html");
-                } else {
-                    //There is an error. Try again
-                    renderTemplate("Application/deleteAccountRetry.html");
                 }
+                //User references in received messages must be previously deleted
+                if(user.receiversList != null){
+                    Message_User mu;
+                    for(int i = 0; i<user.receiversList.size(); i++) {
+                        mu = user.receiversList.get(i);
+                        mu.receiver = null;
+                        mu.save();
+                    }
+                }
+                user.delete();
+
+                //Render login webpage
+                renderTemplate("Application/index.html");
             } else {
-                //There is an error. Try again
+                //Wrong password. Render error
                 renderTemplate("Application/deleteAccountRetry.html");
             }
+        } else {
+            //Username does not exist. Render error
+            renderTemplate("Application/deleteAccountRetry.html");
         }
-        
+    }
+
+    //Query for updating password
     public static void updatePassword(String name, String newPassword, String password) {
+        //Query asks for user given its name (CAMBIAR A MAIL)
         User user = User.find("byFullName", name).first();
+
+        //In case there is a user, check if password is well
         if (user != null) {
+            //If password is well, change to the new one
             if (password.equals(user.password)) {
                 user.password = newPassword;
                 user.save();
+                //AÑADIR RENDER CON MENSAJE EN HTML
             }
+            //PONER ELSE Y AÑADIR RENDER EN HTML
             renderText("Wrong password.");
         }
+        //AÑADIR ERROR SI NO HAY USUARIO
     }
 
+    //Query for updating name
     public static void updateName(String name, String newName, String password) {
+        //Query asks for user given its name (CAMBIAR A MAIL)
         User user = User.find("byFullName", name).first();
+
+        //In case there is a user, check if password is well
         if (user != null) {
+            //If password is well, change to the new name
             if (password.equals(user.password)) {
                 user.fullName = newName;
                 user.save();
+                //AÑADIR RENDER CON MENSAJE EN HTML
             }
+            //PONER ELSE Y AÑADIR RENDER EN HTML
             renderText("Wrong password.");
         }
+        //AÑADIR ERROR SI NO HAY USUARIO
     }
 
+    //Query for sending a message to other users
     public static void send (String subject, String message, String receiverMail) {
+        //Save the message on database
         Message mess = new Message(subject,message);
         mess.save();
+
+        //Queries for getting sender and receiver users given their mails
         User user1 = User.find("byMail", connectedUser).first();
         User user2 = User.find("byMail", receiverMail).first();
+
+        //In case users are found, get current date and send message
         if (user1 != null && user2 != null) {
             Date hoy = new Date();
             new Message_User( mess,  user1,  user2,  "main", hoy,  false).save();
             renderTemplate("Application/inbox.html");
         }
+        //In rest of cases, there is an error. Render message
         else {
             renderTemplate("Application/sendMessageRetry.html");
             mess.delete();
         }
     }
 
+    //Query for getting a user's inbox
     public static void getInbox(String mail, String inboxCode) {
         //localhost:9000/Application/getInbox?mail=j@gmail.com&inboxCode=main
+
+        //Query asks for messages with the given inbox code for the given receiver
+        //AÑADIR QUERY PARA BANDEJA ENVIADOS Y COMPLETAR QUERY CON MAIL Y FECHA
         Query query = JPA.em().createQuery("SELECT m.message FROM Message_User m WHERE m.receiver.mail " +
                         "LIKE :receiverMail AND m.inbox LIKE :inboxCode")
                 .setParameter("receiverMail", mail)
                 .setParameter("inboxCode", inboxCode);
         List<Message> messages = query.getResultList();
+
+        //Cast results to JSON
         Gson g = new Gson();
         g = g.newBuilder().excludeFieldsWithoutExposeAnnotation().create();
         renderJSON(  g.toJson(messages));
+        //AÑADIR RESUPESTA ERROR SI NO HAY MENSAJES
     }
 }
